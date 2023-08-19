7fcf42b44b480341cf326afe40d32ea63042887b
Nurullo <ghostone234qwer.gmail.com>
2023-08-19 23:55:44 +0500

    add need entities for project

---
+++ OLMS.Domain/Commons/Auditable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OLMS.Domain.Commons;
+
+public abstract class Auditable
+{
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ OLMS.Domain/Entities/CourseCategories/CourseCategory.cs
@@ -0,0 +1,21 @@
+﻿using OLMS.Domain.Commons;
+using System.Collections.Generic;
+
+namespace OLMS.Domain.Entities.CourseCategories;
+
+public class CourseCategory : Auditable
+{
+    public string Name { get; set; }
+    public ICollection<Course> Categories { get; set; }
+}
+
+public class Course : Auditable
+{
+    public string Name { get; set; }
+    public string ShortDescription { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+
+    public long CourseId { get; set; }
+    public CourseCategory CourseCategory { get; set; }
+}

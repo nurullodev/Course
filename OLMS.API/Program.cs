5863a57dfc045a691bfc2475d157e7b6b5cff953
Nurullo <ghostone234qwer.gmail.com>
2023-08-20 19:25:51 +0500

    change main project

--- OLMS.API/Program.cs
+++ OLMS.API/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using OLMS.DAL.DbContexts;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,11 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseNpgsql(builder.Configuration
-        .GetConnectionString("DefaultConnection"));
-});
 
 var app = builder.Build();
 
